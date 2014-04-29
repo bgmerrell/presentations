438cc68fa94d7ace42ed712f14dd55bb1c159870
Brian G. Merrell <brian.merrell@mediaforge.com>
2014-04-28 21:05:20 -0600

    Add missing code file

---
+++ intro-to-go/concurrency-for-with-select.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    tick := time.Tick(1 * time.Second)
+    boom := time.After(5 * time.Second)
+    for {
+        select {
+        case <-tick:
+            fmt.Println("tick.")
+        case <-boom:
+            fmt.Println("BOOM!")
+            return
+        default:
+            fmt.Println("    .")
+            time.Sleep(500 * time.Millisecond)
+        }
+    }
+}
