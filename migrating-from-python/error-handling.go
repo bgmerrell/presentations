892959a2589ef9925a33cf9b44a661e5430f7102
Brian G. Merrell <brian.merrell@mediaforge.com>
2014-04-28 21:04:44 -0600

    Introduce migraton-from-python presentation

---
+++ migrating-from-python/error-handling.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type FooError struct {
+	When time.Time
+	What string
+}
+
+func (e *FooError) Error() string {
+	return fmt.Sprintf("at %v, %s", e.When, e.What)
+}
+
+type BarError struct{}
+
+func (e *BarError) Error() string {
+	return "BarError"
+}
+
+func run() error {
+	return &FooError{
+		time.Now(),
+		"it didn't work",
+	}
+}
+
+func main() {
+	var err error
+	err = run()
+	if _, ok := err.(*BarError); ok {
+		fmt.Println(err)
+	}
+
+	fmt.Println("...")
+
+	if _, ok := err.(*FooError); ok {
+		fmt.Println(err)
+	}
+}
