7398e3e36855914adb94b280e985832b3504dc9e
Brian G. Merrell <brian.merrell@mediaforge.com>
2014-04-30 01:20:13 -0600

    Continue work on migrating-from-python slides

--- migrating-from-python/error-handling.go
+++ migrating-from-python/error-handling.go
@@ -5,38 +5,24 @@ import (
 	"time"
 )
 
-type FooError struct {
-	When time.Time
-	What string
-}
-
+// START OMIT
+type FooError struct { When time.Time; What string }
 func (e *FooError) Error() string {
-	return fmt.Sprintf("at %v, %s", e.When, e.What)
+	return fmt.Sprintf("at %s, %s", e.When, e.What)
 }
 
 type BarError struct{}
-
-func (e *BarError) Error() string {
-	return "BarError"
-}
+func (e *BarError) Error() string { return "BarError" }
 
 func run() error {
-	return &FooError{
-		time.Now(),
-		"it didn't work",
-	}
+	return &FooError{ time.Now(), "it didn't work", }
 }
 
 func main() {
 	var err error
 	err = run()
-	if _, ok := err.(*BarError); ok {
-		fmt.Println(err)
-	}
-
+	if _, ok := err.(*BarError); ok { fmt.Println(err) }
 	fmt.Println("...")
-
-	if _, ok := err.(*FooError); ok {
-		fmt.Println(err)
-	}
+	if _, ok := err.(*FooError); ok { fmt.Println(err) }
 }
+// END OMIT
