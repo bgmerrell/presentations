5f16e1b86e1b1c68095da238eb67c2872502ca9a
Brian G. Merrell <bmerrell@walmart.com>
2017-03-08 11:57:55 -0800

    Updates for 3/8 presentation

--- programming-in-go/variable-declarations.go
+++ programming-in-go/variable-declarations.go
@@ -7,9 +7,11 @@ func main() {
 	var i, j int = 1, 2
 	k := 3.2
 	a, b, c, d := true, false, "no!", '氣'
+	var e int
 
 	fmt.Println(i, j, k, a, b, c, d)
 	fmt.Printf("%T, %T, %T, %T, %T, %T, %T\n", i, j, k, a, b, c, d)
+	fmt.Println(e)
 }
 
 // END OMIT
